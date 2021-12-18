186dffcc64b70d0ad71ee5582bd242743d111fca
dobly12 <96325422+dobly12@users.noreply.github.com>
2021-12-18 10:55:41 +0700

    Add files via upload

---
+++ asikin/fisrt.php
@@ -0,0 +1,27 @@
+<html>
+<body background="/asikin/apel.jfif">
+	<center>	
+		<h1>My First Coding</h1>
+<?php
+echo'<body style="background-image=/asikin/apel.jfif">';
+echo"aku mau makan";
+echo"<br>";
+$x="5";
+echo"$x apel";
+echo"<br>";
+echo'<img src="/asikin/apel.jfif"width="1000"height="500"/>';
+echo"<br>";
+echo"$x pepaya";
+echo"<br>";
+echo"lunpia";
+echo"<br>";
+$makan="bakmie jowo";
+echo"Renald pergi jajan $makan";
+echo"<br>";
+echo"ooh beli $makan";
+echo"<br>";
+echo"mami pergi $makan";
+?>
+</center>
+</body>
+</html>
